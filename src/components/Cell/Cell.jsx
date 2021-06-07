6da5413666acfd1f3f83e35277d3b32484c78040
dani <danicampu56@gmail.com>
2021-06-06 23:19:43 -0300

    second commit ?

---
+++ src/components/Cell/Cell.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {StyledCell} from './StyledCell';
+import {TETROMINOS} from '../../tetrominos';
+
+const Cell = ({ type }) => {
+  return <StyledCell type={'L'} color={TETROMINOS['L'].color} />
+}
+
+export default Cell;
---
+++ src/components/Display/Display.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {StyledDisplay} from './StyledDisplay';
+const Display = ({gameOver, text}) => {
+  return <StyledDisplay>{text}</StyledDisplay>;
+}
+
+export default Display;
