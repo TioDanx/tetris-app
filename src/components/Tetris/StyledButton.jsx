897bd17961184333c2ea8c27cab15bcb9e51f896
dani <danicampu56@gmail.com>
2021-06-11 14:03:20 -0300

    Fixed more issues

---
+++ src/components/Tetris/StyledButton.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const StyledButton = styled.button`
+  outline: none;
+  text-decoration: none;
+  color: grey;
+  background-color: inherit;
+  width: 100%;
+  height: 100%;
+  border: none;
+  cursor: pointer;
+  font-size: 14px;
+  font-family: inherit;
+`
